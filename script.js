93a915891a536e9df0fa38c3d78d3435d5ca4f15
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-08-21 20:07:48 +0530

    Added 2nd challenge

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+	var welcome = document.getElementById('welcome');
+	welcome.innerHTML = "welcome back :)";
+	var flag1 = "OZCTF{S@Y_n0_t0_def@ult_creds_in_c0mments}";
+}
